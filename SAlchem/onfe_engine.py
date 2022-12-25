5e3f84b9d82963add09b97c8f4c43aa8bd879234
boringbyte <kottapallisivaram@gmail.com>
2022-12-25 15:14:49 -0500

    fast-api documentation

--- SAlchem/onfe_engine.py
+++ SAlchem/onfe_engine.py
@@ -1,9 +1,21 @@
 # https://www.youtube.com/watch?v=1Va493SMTcY
 # https://github.com/zzzeek/sqla_tutorial/tree/master/slides
+import os
 from sqlalchemy import create_engine, text
 
 
+if os.path.exists('some.db'):
+    os.remove('some.db')
+
+
+# Create Table
 engine = create_engine('sqlite:///some.db', future=True)
+with engine.begin() as conn:
+    conn.execute(text("CREATE TABLE employee (emp_id INTEGER PRIMARY KEY, emp_name VARCHAR)"))
+    conn.execute(text("CREATE TABLE employee_of_month (emp_id INTEGER PRIMARY KEY, emp_name VARCHAR)"))
+    conn.execute(text("INSERT INTO employee(emp_name) VALUES (:name)"),
+                 [{"name": "spongebob"}, {"name": "sandy"}, {"name": "squidward"}],)
+
 print(type(engine))  # Checking what type of engine are we using?
 connection = engine.connect()  # We are now connecting to the DB and then some.db file gets created.
 print(connection)
@@ -19,25 +31,32 @@ print(row.emp_id)
 print(row['emp_name'])
 print(row[0])
 print(row._mapping["emp_name"])
+result = connection.execute(stmt, {"emp_id": 2})
 for emp_id, emp_name in result:
     print(f'employee id: {emp_id}   employee name: {emp_name}')
 connection.close()
 
 
+"""
 # 3 ways of connecting
 with engine.connect() as conn:
     # write the sql statements. This doesn't autocommit
-    pass
+    conn.execute(text("INSERT INTO employee(emp_name) VALUES (:name)"),
+                 [{"name": "spongebob"}, {"name": "sandy"}, {"name": "squidward"}],)
+    conn.commit()
 
 with engine.begin() as conn:
     # write the sql statements. This does autocommit and closes the connection.
-    pass
+    conn.execute(text("INSERT INTO employee(emp_name) VALUES (:name)"),
+             [{"name": "spongebob"}, {"name": "sandy"}, {"name": "squidward"}],)
 
 with engine.connect() as conn:
     with conn.begin():
         # write the sql statements. This does autocommit and comes out of the transaction.
-        conn.execute(stmt)
+        conn.execute(text("INSERT INTO employee(emp_name) VALUES (:name)"),
+             [{"name": "spongebob"}, {"name": "sandy"}, {"name": "squidward"}],)
     # But still maintains the connection
+"""
 
 if __name__ == '__main__':
     pass
